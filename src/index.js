c0a08a7d35ddc7084385fe5804a89246568ce8e4
Craig Miller <craigmiller160@gmail.com>
2020-06-15 09:51:42 -0400

    Removed cloud config init

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ const startExpress = require('./express');
 const { cloudConfig, logger } = require('@craigmiller160/covid-19-config-mongo');
 
 logger.info('Starting application');
-cloudConfig.init()
-    .then(startExpress)
-    .catch((ex) => {
-        logger.error('Critical error starting application');
-        logger.error(ex);
-    });
+startExpress();
