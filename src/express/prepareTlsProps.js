ad7c997e6c3ba4402c59766ffb41375f046ac999
Craig Miller <craigmiller160@gmail.com>
2020-12-13 16:49:54 -0500

    Added TLS ciphers

--- src/express/prepareTlsProps.js
+++ src/express/prepareTlsProps.js
@@ -19,7 +19,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const ciphers = [
+    'ECDHE-ECDSA-AES256-GCM-SHA384',
+    'ECDHE-RSA-AES256-GCM-SHA384',
+    'ECDHE-ECDSA-CHACHA20-POLY1305',
+    'ECDHE-RSA-CHACHA20-POLY1305',
+    'ECDHE-ECDSA-AES128-GCM-SHA256',
+    'ECDHE-RSA-AES128-GCM-SHA256',
+    'ECDHE-ECDSA-AES256-SHA384',
+    'ECDHE-RSA-AES256-SHA384',
+    'ECDHE-ECDSA-AES128-SHA256',
+    'ECDHE-RSA-AES128-SHA256'
+];
+
 module.exports = {
     key: fs.readFileSync(path.resolve(__dirname, '../cert/key.pem')),
-    cert: fs.readFileSync(path.resolve(__dirname, '../cert/certificate.pem'))
+    cert: fs.readFileSync(path.resolve(__dirname, '../cert/certificate.pem')),
+    ciphers: ciphers.join(';')
 };
