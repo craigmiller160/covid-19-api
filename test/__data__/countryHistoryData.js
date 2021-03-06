04ca899d3d4b843a75c0b80567e7e3fa66f1347d
Craig Miller <craigmiller160@gmail.com>
2021-03-06 13:33:52 -0500

    Setting up test data

---
+++ test/__data__/countryHistoryData.js
@@ -0,0 +1,188 @@
+/*
+ *     covid-19-api
+ *     Copyright (C) 2021 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = [
+    {
+        date: new Date('2020-01-22T05:00:00Z'),
+        location: 'USA',
+        newCases: 555,
+        newDeaths: 17,
+        totalCases: 555,
+        totalDeaths: 17,
+        rawDate: '1/22/20',
+        newVaccines: 0,
+        totalVaccines: 0,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-23T05:00:00Z'),
+        location: 'USA',
+        newCases: 99,
+        newDeaths: 1,
+        totalCases: 654,
+        totalDeaths: 18,
+        rawDate: '1/23/20',
+        newVaccines: 0,
+        totalVaccines: 0,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-24T05:00:00Z'),
+        location: 'USA',
+        newCases: 287,
+        newDeaths: 8,
+        totalCases: 941,
+        totalDeaths: 26,
+        rawDate: '1/24/20',
+        newVaccines: 10,
+        totalVaccines: 10,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-25T05:00:00Z'),
+        location: 'USA',
+        newCases: 493,
+        newDeaths: 16,
+        totalCases: 1434,
+        totalDeaths: 42,
+        rawDate: '1/25/20',
+        newVaccines: 10,
+        totalVaccines: 20,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-26T05:00:00Z'),
+        location: 'USA',
+        newCases: 684,
+        newDeaths: 14,
+        totalCases: 2118,
+        totalDeaths: 56,
+        rawDate: '1/26/20',
+        newVaccines: 10,
+        totalVaccines: 30,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-27T05:00:00Z'),
+        location: 'USA',
+        newCases: 809,
+        newDeaths: 26,
+        totalCases: 2927,
+        totalDeaths: 82,
+        rawDate: '1/27/20',
+        newVaccines: 15,
+        totalVaccines: 45,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-28T05:00:00Z'),
+        location: 'USA',
+        newCases: 2651,
+        newDeaths: 49,
+        totalCases: 5578,
+        totalDeaths: 131,
+        rawDate: '1/28/20',
+        newVaccines: 5,
+        totalVaccines: 50,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-22T05:00:00Z'),
+        location: 'UK',
+        newCases: 554,
+        newDeaths: 16,
+        totalCases: 554,
+        totalDeaths: 16,
+        rawDate: '1/22/20',
+        newVaccines: 0,
+        totalVaccines: 0,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-23T05:00:00Z'),
+        location: 'UK',
+        newCases: 99,
+        newDeaths: 1,
+        totalCases: 654,
+        totalDeaths: 18,
+        rawDate: '1/23/20',
+        newVaccines: 0,
+        totalVaccines: 0,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-24T05:00:00Z'),
+        location: 'UK',
+        newCases: 287,
+        newDeaths: 8,
+        totalCases: 941,
+        totalDeaths: 26,
+        rawDate: '1/24/20',
+        newVaccines: 10,
+        totalVaccines: 10,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-25T05:00:00Z'),
+        location: 'UK',
+        newCases: 493,
+        newDeaths: 16,
+        totalCases: 1434,
+        totalDeaths: 42,
+        rawDate: '1/25/20',
+        newVaccines: 10,
+        totalVaccines: 20,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-26T05:00:00Z'),
+        location: 'UK',
+        newCases: 684,
+        newDeaths: 14,
+        totalCases: 2118,
+        totalDeaths: 56,
+        rawDate: '1/26/20',
+        newVaccines: 10,
+        totalVaccines: 30,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-27T05:00:00Z'),
+        location: 'UK',
+        newCases: 809,
+        newDeaths: 26,
+        totalCases: 2927,
+        totalDeaths: 82,
+        rawDate: '1/27/20',
+        newVaccines: 15,
+        totalVaccines: 45,
+        population: 10000000
+    },
+    {
+        date: new Date('2020-01-28T05:00:00Z'),
+        location: 'UK',
+        newCases: 2650,
+        newDeaths: 48,
+        totalCases: 5577,
+        totalDeaths: 130,
+        rawDate: '1/28/20',
+        newVaccines: 5,
+        totalVaccines: 50,
+        population: 10000000
+    }
+];
