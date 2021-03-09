92254244972d17280a744dfebfd1c8803ccc871d
Craig <craigmiller160@gmail.com>
2021-03-08 19:28:54 -0500

    Improved error handling

--- src/service/CountryCompareService.js
+++ src/service/CountryCompareService.js
@@ -1,15 +1,18 @@
 const { connect } = require('@craigmiller160/covid-19-config-mongo');
 const TraceError = require('trace-error');
-const moment = require('moment');
 
 const COLLECTION = 'country_compare';
 
 const getCountryCompareData = async () => {
-    return connect(async (db) =>
-        await db.collection(COLLECTION)
-            .find()
-            .toArray()
-    );
+    try {
+        return await connect(async (db) =>
+            await db.collection(COLLECTION)
+                .find()
+                .toArray()
+        );
+    } catch (ex) {
+        throw new TraceError('Error getting country compare data', ex);
+    }
 };
 
 module.exports = {
